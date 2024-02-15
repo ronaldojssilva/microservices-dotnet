c8fed4ba1abb176efed82b89c3d9843c42043f7d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-15 14:11:32 -0300

    Configuring Product API microservice access to the database

--- 07_Microservices.Net-FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ 07_Microservices.Net-FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,8 +1,19 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(connection,
+                     new MySqlServerVersion(new Version(8, 2, 0)
+    )));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
