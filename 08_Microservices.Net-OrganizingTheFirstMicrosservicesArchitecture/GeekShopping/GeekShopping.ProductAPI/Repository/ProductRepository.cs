eb130409a0f586858402af01780f439633c1430c
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-15 21:44:10 -0300

    Improving the architecture

--- 08_Microservices.Net-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ 08_Microservices.Net-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -26,7 +26,7 @@ namespace GeekShopping.ProductAPI.Repository
         public async Task<ProductVO> FindById(long id)
         {
             Product product = await _context.Products.Where(p => p.Id == id)
-                                                     .FirstOrDefaultAsync();
+                                                     .FirstOrDefaultAsync() ?? new Product();
             return _mapper.Map<ProductVO>(product);
 
         }
@@ -52,8 +52,8 @@ namespace GeekShopping.ProductAPI.Repository
             try
             {
                 Product product = await _context.Products.Where(p => p.Id == id)
-                                         .FirstOrDefaultAsync();
-                if (product == null) return false;
+                                         .FirstOrDefaultAsync() ?? new Product();
+                if (product.Id <= 0) return false;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync ();
                 return true;
