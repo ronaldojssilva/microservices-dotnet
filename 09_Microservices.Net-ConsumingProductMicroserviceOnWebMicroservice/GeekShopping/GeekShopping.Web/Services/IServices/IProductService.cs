3c45e2603a4ddd2be3be6f8be99cb31a7454d357
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-16 08:35:53 -0300

    Defining the Operations of the IProductService Interface

---
+++ 09_Microservices.Net-ConsumingProductMicroserviceOnWebMicroservice/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAll();
+
+        Task<ProductModel> FindById(long id);
+
+        Task<ProductModel> Create(ProductModel model);
+
+        Task<ProductModel> Update(ProductModel model);
+
+        Task<bool> DeleteById(long id);
+    }
+}
