9cdc56f09265cef62a936b37f1df9aa2d7aff68a
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-16 09:28:54 -0300

    Implementing the ProductService Class

---
+++ 09_Microservices.Net-ConsumingProductMicroserviceOnWebMicroservice/GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public async Task<IEnumerable<ProductModel>> FindAll()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> Create(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductModel> Update(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteById(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
