66a2cd04eb517c9d19580c60cddf6600580eb05d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-21 17:32:01 -0300

    Implementing the IdentityConfiguration class

---
+++ 11_Microservices.Net-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/geekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace geekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        //perfis de usuarios
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        //Identity resource
+        //Informações relacionadas à identidade do usuário. Ex: Nome, e-mail etc
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+
+        //Api Scope. Identificadores ou rescursos que um client pode acessar. Ex.: GeekShopping.Web
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> 
+            { 
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name:"read", "Read data."),
+                new ApiScope(name:"write", "Write data."),
+                new ApiScope(name:"delete", "Delete data."),
+
+            };
+
+        //Clients
+        public static IEnumerable<Client> Clients =>
+            new List<Client> 
+            { 
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile"},
+                }
+            };
+    }
+}
