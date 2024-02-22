a998f77189fad4b1abeb5e6e15eec078782e553e
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-22 11:36:24 -0300

    Adding Database and Security Settings

--- 11_Microservices.Net-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/geekShopping.IdentityServer/Program.cs
+++ 11_Microservices.Net-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/geekShopping.IdentityServer/Program.cs
@@ -1,4 +1,34 @@
+using geekShopping.IdentityServer.Configuration;
+using geekShopping.IdentityServer.Model;
+using geekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(connection,
+                     new MySqlServerVersion(new Version(8, 2, 0)
+    )));
+
+//Services Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityUser>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var geekIdentiyBuilder = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+  .AddInMemoryClients(IdentityConfiguration.Clients)
+  .AddAspNetIdentity<ApplicationUser>();
+geekIdentiyBuilder.AddDeveloperSigningCredential();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,6 +43,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
