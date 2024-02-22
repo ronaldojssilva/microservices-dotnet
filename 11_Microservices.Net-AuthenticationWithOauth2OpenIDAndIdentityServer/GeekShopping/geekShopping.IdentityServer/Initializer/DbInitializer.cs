ee6d68d5baf06df1d135c147fd95a47d338fbaa4
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-22 15:52:20 -0300

    Creating the Class Responsible for Initializing the Database

---
+++ 11_Microservices.Net-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/geekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using geekShopping.IdentityServer.Configuration;
+using geekShopping.IdentityServer.Model;
+using geekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace geekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<ApplicationUser> _role;
+
+        public DbInitializer(MySQLContext context, UserManager<ApplicationUser> user, RoleManager<ApplicationUser> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null) return;
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult(); 
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Client)).GetAwaiter().GetResult(); 
+
+        }
+    }
+}
