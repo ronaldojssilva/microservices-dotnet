157221a6ea5a018862156d212aa78dce51be8276
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-23 22:20:48 -0300

    Configuring the Startup Class and DbContext of GeekShopping.CartAPI MS

---
+++ 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/GeekShopping.CartAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class MySQLContext: DbContext
+    {
+        public MySQLContext() { }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options): base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
--- 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/GeekShopping.CartAPI/Program.cs
+++ 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -1,11 +1,85 @@
+using AutoMapper;
+using GeekShopping.CartAPI.Config;
+using GeekShopping.CartAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(connection,
+                     new MySqlServerVersion(new Version(8, 2, 0)
+    )));
+
+//Mapping
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Repositories
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddControllers();
+
+//Security
+//Authentication
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+        };
+    });
+//Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.CartAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +91,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Authentication
+app.UseAuthentication();
 
 app.UseAuthorization();
 
