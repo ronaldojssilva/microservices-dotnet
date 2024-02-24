0d0afefe1a4264419885e749d0d42e4ac09ab50e
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-24 15:56:49 -0300

    Adding the Shopping Cart to the Menu and Creating the CartController

---
+++ 15_Microservices.Net-CreatingShoppingCartUI/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
