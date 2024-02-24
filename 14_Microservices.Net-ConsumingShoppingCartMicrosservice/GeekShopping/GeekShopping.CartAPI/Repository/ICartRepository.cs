e3207f436d1e9601b707cc5fb462303bc1242c1d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-24 13:37:00 -0300

    Consuming Shopping Cart Microsservice

---
+++ 14_Microservices.Net-ConsumingShoppingCartMicrosservice/GeekShopping/GeekShopping.CartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartByUserId(string useId);
+        Task<CartVO> SaveOrUpdate(CartVO cart);
+        Task<bool> RemoveFromCart(long cartDetailId);
+        Task<bool> ApplyCoupon(string userId, string couponCode);
+        Task<bool> RemoveCoupon(string userId);
+        Task<bool> ClearCart(string userId);
+    }
+}
