bb94d76c9e5218a147dec0a99ecd3cdfce570dbd
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-24 14:44:54 -0300

    Implementing Adding Items to Cart

--- 14_Microservices.Net-ConsumingShoppingCartMicrosservice/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ 14_Microservices.Net-ConsumingShoppingCartMicrosservice/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -11,12 +11,14 @@ namespace GeekShopping.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(ILogger<HomeController> logger, IProductService productService, ICartService cartService)
         {
             _logger = logger;
             _productService = productService;
+            _cartService = cartService;   
         }
 
         public async Task<IActionResult> Index()
@@ -31,6 +33,35 @@ namespace GeekShopping.Web.Controllers
         {
             string token = await HttpContext.GetTokenAsync("access_token");
             var model = await _productService.FindById(id, token);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Details")]
+        [Authorize]
+        public async Task<IActionResult> DetailsPost(ProductViewModel model)
+        {
+            string token = await HttpContext.GetTokenAsync("access_token");
+            CartViewModel cart = new CartViewModel()
+            {
+                CartHeader = new CartHeaderViewModel
+                {
+                    UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
+                }
+            };
+            CartDetailViewModel cartDetail = new CartDetailViewModel()
+            {
+                Count = model.Count,
+                ProductId = model.Id,
+                Product = await _productService.FindById(model.Id, token)
+            };
+            List<CartDetailViewModel> cartDetails = new List<CartDetailViewModel>();
+            cartDetails.Add(cartDetail);
+            var response = await _cartService.AddItemToCart(cart, token);
+            if (response != null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
             return View(model);
         }
 
