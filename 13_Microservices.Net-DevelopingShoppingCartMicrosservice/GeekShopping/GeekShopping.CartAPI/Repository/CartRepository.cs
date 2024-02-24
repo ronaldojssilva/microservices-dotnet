42a3b437efb2931970dbfeb262aa35bc89eb62ad
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-24 10:04:04 -0300

    Starting the Implementation of the Repository Classes

---
+++ 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserId(string useId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> SaveOrUpdate(CartVO cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
