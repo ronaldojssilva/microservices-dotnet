00215abbac83f8648103893a0fd7456597a0a48d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-24 20:42:15 -0300

    Consuming GeekShopping Coupon API Microservice

---
+++ 17_Microservices.Net-ConsumingShoppingCouponAPIMicroservice/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : BaseController
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            CartViewModel? response = await FindCart();
+            return View(response);
+        }
+        public async Task<IActionResult> Remove(int id)
+        {
+            string token = await GetAccessToken();
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var response = await _cartService.RemoveFromCart(id, token);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        private async Task<CartViewModel?> FindCart()
+        {
+            string token = await GetAccessToken();
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var response = await _cartService.FindCartByUserId(userId, token);
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    response.CartHeader.PurchaseAmount += detail.Product.Price * detail.Count;
+                }
+            }
+
+            return response;
+        }
+    }
+}
