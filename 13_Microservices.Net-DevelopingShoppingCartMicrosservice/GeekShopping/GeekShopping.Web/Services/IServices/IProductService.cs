2c7e5d89baed21e5b92bff9205372182c79b7721
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-23 21:47:28 -0300

    Developing Shopping Cart Microsservice

---
+++ 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAll(string token);
+
+        Task<ProductModel> FindById(long id, string token);
+
+        Task<ProductModel> Create(ProductModel model, string token);
+
+        Task<ProductModel> Update(ProductModel model, string token);
+
+        Task<bool> DeleteById(long id, string token);
+    }
+}
---
+++ 13_Microservices.Net-DevelopingShoppingCartMicrosservice/GeekShopping/geekShopping.IdentityServer/Program.cs
@@ -0,0 +1,70 @@
+using Duende.IdentityServer.Services;
+using geekShopping.IdentityServer.Configuration;
+using geekShopping.IdentityServer.Initializer;
+using geekShopping.IdentityServer.Model;
+using geekShopping.IdentityServer.Model.Context;
+using geekShopping.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(connection,
+                     new MySqlServerVersion(new Version(8, 2, 0)
+    )));
+
+//Services Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var geekIdentiyBuilder = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+      .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+      .AddInMemoryClients(IdentityConfiguration.Clients)
+      .AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
+
+geekIdentiyBuilder.AddDeveloperSigningCredential();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+var initializer = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializer>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+initializer.Initialize();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
