fa6043e773fef0d28fdfc6a249c8cb93ceecd88d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-25 18:41:09 -0300

    Configuring the Startup and DbContext of the GeekShopping.OrderAPI microservice

---
+++ 20_Microservices.Net-CreatingOrderAPIMicroservice/GeekShopping/GeekShopping.OrderAPI/Model/OrderHeader.cs
@@ -0,0 +1,40 @@
+﻿using GeekShopping.OrderAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model
+{
+    [Table("Order_header")]
+    public class OrderHeader : BaseEntity
+    {
+        [Column("user_d")]
+        public string UserId { get; set; }
+
+        [Column("coupon_code")]
+
+        public string CouponCode { get; set; }
+
+        public decimal PurchaseAmount { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string CVV { get; set; }
+
+        public string ExpiryMonthYear { get; set; }
+
+        public int CartTotalItens { get; set; }
+
+        public IEnumerable<OrderDetail>? CartDetails { get; set; }
+    }
+}
--- 20_Microservices.Net-CreatingOrderAPIMicroservice/GeekShopping/GeekShopping.OrderAPI/Program.cs
+++ 20_Microservices.Net-CreatingOrderAPIMicroservice/GeekShopping/GeekShopping.OrderAPI/Program.cs
@@ -1,11 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+/ DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"] ;
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(connection,
+                     new MySqlServerVersion(new Version(8, 2, 0)
+    )));
+
+//Mapping
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Repositories
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
+
 builder.Services.AddControllers();
+
+//Security
+//Authentication
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+        };
+    });
+//Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.OrderAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +90,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
