3c1a9bc71206c712d467ccc5de845820d809abca
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-26 11:29:11 -0300

    Adding the Necessary VO's to GeekShopping.CouponAPI Consumption

---
+++ 21_Microservices.Net-SyncCommunicationBetweenMicroservices/GeekShopping/GeekShopping.CartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CouponRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponVO> GetByCouponCode(string code, string token)
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Code == code);
+            return _mapper.Map<CouponVO>(coupon);
+        }
+    }
+}
