067b1bd1e1b3edee1ea7c5ef5652ffd0692e139e
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-27 13:02:55 -0300

    Working With Exchanges In RabbitMQ-Fanout

---
+++ 23_Microservices.Net-WorkingWithExchangesInRabbitMQ-Fanout/GeekShopping/GeekShopping.CartAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponVO> GetByCoupon(string couponCode, string token);
+    }
+}
