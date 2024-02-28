9b67047834159489ec5c436bf3d6a60921222e0a
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-28 11:18:13 -0300

    Defining Base URL and Security Settings in Ocelot

--- 24_Microservices.Net-WorkingWithOcelot/GeekShopping/GeekShopping.ApiGateway/Program.cs
+++ 24_Microservices.Net-WorkingWithOcelot/GeekShopping/GeekShopping.ApiGateway/Program.cs
@@ -1,17 +1,36 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+//Add Ocelot
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+        };
+    });
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
